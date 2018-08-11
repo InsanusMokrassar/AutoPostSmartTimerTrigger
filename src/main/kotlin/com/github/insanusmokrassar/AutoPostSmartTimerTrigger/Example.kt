4262a97fc71bc9270b44d42c49c695ddeddeefd5
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-08-11 17:01:12 +0600

    update an example

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostSmartTimerTrigger/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostSmartTimerTrigger/Example.kt
@@ -4,9 +4,12 @@ import com.github.insanusmokrassar.IObjectKRealisations.load
 import com.github.insanusmokrassar.IObjectKRealisations.toObject
 
 fun main(args: Array<String>) {
-    load(args[0]).toObject(SmartTimerConfig::class.java).also {
-        println(
-            it.triggerTimes.joinToString("\n")
-        )
+    load(args[0]).also {
+        println(it)
     }
+        .toObject(SmartTimerConfig::class.java).also {
+            println(
+                it.triggerTimes.joinToString("\n")
+            )
+        }
 }
